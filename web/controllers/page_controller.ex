4f2bf62f7c42bf1bc37d1daf1c20be4cfb1e77ff
ayimdomnic <ayimdomnic@outlook.com>
2017-03-05 18:58:51 +0300

    Fresh Installation of Phoenix with MySQL

---
+++ web/controllers/page_controller.ex
@@ -0,0 +1,10 @@
+defmodule Web.PageController do
+  use Web.Web, :controller
+
+  def index(conn, _params) do
+    render conn, "index.html"
+  end
+  def about(conn, _params) do
+    render conn, "about.html"
+  end
+end
